0a2184bc03cafd96ed35c70a81a05311f8dfb2bc
Takumi Sueda <puhitaku@gmail.com>
2019-06-05 16:24:55 +0900

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$(pwd)" != *rcs ]]; then
+    echo "Please cd into rcs repository root before running this."
+    exit 1
+fi
+
+linux=$(uname | grep -q Linux && echo 0)
+mac=$(uname | grep -q Darwin && echo 0)
+
+which curl || sudo apt install curl
+which pyenv || curl https://pyenv.run | bash
+
+ln -s $(pwd)/rc.sh ~/rc.sh
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+if [ $linux ]; then
+    ln -s $(pwd)/.tmux.conf.linux ~/.tmux.conf.linux
+elif [ $mac ]; then
+    ln -s $(pwd)/.tmux.conf.mac ~/.tmux.conf.mac
+else
+    echo "Warning: no OS-dependent .tmux.conf is not installed for $(uname)."
+fi
+
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.inputrc ~/.inputrc
+
