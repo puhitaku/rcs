7636c7d7ee86d997d80c54f8357c21c750d8239c
Takumi Sueda <puhitaku@gmail.com>
2021-12-21 16:47:38 +0900

    Add mtplvcap

--- rc.sh
+++ rc.sh
@@ -25,6 +25,7 @@ export PATH=$PATH:/usr/lib/go-1.1?/bin
 export PATH=$PATH:$HOME/.cargo/bin
 export PATH=$PATH:$HOME/.local/kitty.app/bin
 export PATH=$PATH:$RCS_DIR/scripts
+export PATH=$PATH:$HOME/dev/mtplvcap
 
 export PYENV_ROOT="$HOME/.pyenv"
 
